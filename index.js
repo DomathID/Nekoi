8bdf778b1a5ea0182a3dfcc567fc1d75101311dd
Bang Domath <bangdompret@gmail.com>
2020-07-21 04:51:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ bot.on('ready', () => {
 });
 const fs = require('fs');
 bot.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./command/').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
-    const command = require(`./command/${file}`);
+    const command = require(`./commands/${file}`);
     bot.commands.set(command.name, command);
 }
 bot.on('message', message => {
